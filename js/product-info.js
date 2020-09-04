47238e7a32d2b371d82f7abab5e532d85826baae
meferbau <eugeferreira95@gmail.com>
2020-09-04 17:21:17 -0300

    se agregan comentarios y productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -39,25 +39,66 @@ document.addEventListener("DOMContentLoaded", function(e){
             categoryNameHTML.innerHTML = category.name;
             categoryDescriptionHTML.innerHTML = category.description;
             productCountHTML.innerHTML = category.soldCount;
-            //productSimilarHTML.innerHTML = category.relatedProducts;
+
 
             //Muestro las imagenes en forma de galería
             showImagesGallery(category.images);
+          }
 
-            getJSONData(PRODUCTS_URL).then(function(resultObj){
-                if (resultObj.status === "ok")
+
+            // Tomo el json de los productos para obtener la información de los productos relacionados
+            getJSONData(PRODUCTS_URL).then(function(resultado){
+                if (resultado.status === "ok")
                 {
-                    productosTodos = resultObj.data;
-                    for (var i=0;i<2;i++){
-                      var productosRelacionados = productosTodos[category.relatedProducts[i]];
-                      productSimilarHTML.innerHTML = productosRelacionados;
-                    }
-                    document.getElementById("productSimilar").innerHTML = prodRelacionados ;
+                  productosTodos = resultado.data;
+                  var primerProductoRelacionado = productosTodos[category.relatedProducts[0]];
+                  var segundoProductoRelacionado = productosTodos[category.relatedProducts[1]];
 
+                  //Obtengo y muestro info del primer elemento en el html
+                  let contHTMLPrimProdRel = "";
+                  contHTMLPrimProdRel += `
+                  <div class="card-body">
+                    <img class="card-img-top" src="`+ primerProductoRelacionado.imgSrc +`" alt="Card image cap">
+                    <h5 id="nombrePrimImg" class="card-title">`+ primerProductoRelacionado.name +`</h5>
+                    <p class="card-text">`+ primerProductoRelacionado.description +`</p>
+                  </div>
+                  `
+                  document.getElementById("primerProdRel").innerHTML = contHTMLPrimProdRel;
+
+                  //Obtengo y muestro info del segundo elemento en el html
+                  let descripcionSegundo = "";
+                  descripcionSegundo = segundoProductoRelacionado.description.substring(0,57);
+                  descripcionSegundo += " ...";
+                  let contHTMLSegProdRel = "";
+                  contHTMLSegProdRel += `
+                  <div class="card-body">
+                    <img class="card-img-top" src="`+ segundoProductoRelacionado.imgSrc +`" alt="Card image cap">
+                    <h5 id="nombrePrimImg" class="card-title">`+ segundoProductoRelacionado.name +`</h5>
+                    <p class="card-text">`+ descripcionSegundo +`</p>
+                  </div>
+                  `
+                  document.getElementById("segundoProdRel").innerHTML = contHTMLSegProdRel;
                 }
+              });
+
+                getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(contenido){
+                    if (contenido.status === "ok")
+                    {
+                      dataComentarios = contenido.data;
+                      let contenidoComentarios = "";
+                      for (i = 0; i<dataComentarios.length; i++){
+                        contenidoComentarios += `
+                        <div>
+                          <p><strong>`+ dataComentarios[i].user +`: </strong>` + dataComentarios[i].description +`</p>
+                          <p>`+ dataComentarios[i].dateTime +`</p>
+                          <hr>
+                        </div>
+                        `
+
+                      }
+                      document.getElementById("comentarios").innerHTML = contenidoComentarios;
+
+                    }
             });
-        }
-    });
-
-
+        });
 });
