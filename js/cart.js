177f559a8e343dc7717d5d2257818eed049474b6
meferbau <eugeferreira95@gmail.com>
2020-10-04 21:14:29 -0300

    entrega 5 - falta forma de pago

--- js/cart.js
+++ js/cart.js
@@ -2,5 +2,85 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  getJSONData(CART_INFO_URL).then(function(resultObj){
+      if (resultObj.status === "ok")
+      {
+          datosCarrito = resultObj.data;
+
+
+          let nombreArtiHTML  = document.getElementById("nombreArticulo");
+          let costoArtiHTML  = document.getElementById("costoArticulo");
+          let cantidadArtiHTML  = document.getElementById("cantidadArticulo");
+
+          let porcentajeEnvio = 0.15;
+
+          document.getElementById("imgArticulo").innerHTML = `<img src="`+datosCarrito.articles[0].src+`" class="img-thumbnail" width="40%">`
+
+          nombreArtiHTML.innerHTML = datosCarrito.articles[0].name;
+          costoArtiHTML.innerHTML = datosCarrito.articles[0].currency +" "+ datosCarrito.articles[0].unitCost;
+
+          cantidadArtiHTML.innerHTML = `<input id="inputCantArticulos" class="quantity" min="1" name="quantity"
+          value="`+ datosCarrito.articles[0].count +`" type="number">`;
+
+          let subTotInicial = 0;
+          subTotInicial = datosCarrito.articles[0].unitCost * datosCarrito.articles[0].count;
+          document.getElementById("subtotalArticulo").innerHTML = datosCarrito.articles[0].currency +" "+ subTotInicial;
+
+          let subTotalHTML = document.getElementById("subTotalHTML");
+          subTotalHTML.innerHTML = datosCarrito.articles[0].currency +" "+ subTotInicial;
+
+          document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotInicial*porcentajeEnvio);
+
+          let subTotSecundario = 0;
+
+          document.getElementById("costoTotal").innerHTML = Math.round(subTotInicial*porcentajeEnvio + subTotInicial);
+
+          document.getElementById("inputCantArticulos").addEventListener("change",function(){
+            let cantDeseada = document.getElementById("inputCantArticulos").value;
+
+            subTotSecundario = datosCarrito.articles[0].unitCost * cantDeseada;
+            document.getElementById("subtotalArticulo").innerHTML = datosCarrito.articles[0].currency +" "+ subTotSecundario;
+
+            document.getElementById("subTotalHTML").innerHTML = datosCarrito.articles[0].currency +" "+ subTotSecundario;
+
+            document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotSecundario*porcentajeEnvio);
+
+            document.getElementById("costoTotal").innerHTML = Math.round(subTotSecundario*porcentajeEnvio + subTotSecundario);
+          });
+
+          document.getElementById("premiumradio").addEventListener("change",function(){
+            porcentajeEnvio = 0.15;
+            if (subTotSecundario != 0){
+              document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotSecundario*porcentajeEnvio);
+              document.getElementById("costoTotal").innerHTML = Math.round(subTotSecundario*porcentajeEnvio + subTotSecundario);
+            }else{
+              document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotInicial*porcentajeEnvio);
+              document.getElementById("costoTotal").innerHTML = Math.round(subTotInicial*porcentajeEnvio + subTotInicial);
+            }
+          });
+          document.getElementById("expressradio").addEventListener("change",function(){
+            porcentajeEnvio = 0.07;
+            if (subTotSecundario != 0){
+              document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotSecundario*porcentajeEnvio);
+              document.getElementById("costoTotal").innerHTML = Math.round(subTotSecundario*porcentajeEnvio + subTotSecundario);
+            }else{
+              document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotInicial*porcentajeEnvio);
+              document.getElementById("costoTotal").innerHTML = Math.round(subTotInicial*porcentajeEnvio + subTotInicial);
+            }
+          });
+          document.getElementById("standardradio").addEventListener("change",function(){
+            porcentajeEnvio = 0.05;
+            if (subTotSecundario != 0){
+              document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotSecundario*porcentajeEnvio);
+              document.getElementById("costoTotal").innerHTML = Math.round(subTotSecundario*porcentajeEnvio + subTotSecundario);
+            }else{
+              document.getElementById("costoEnvioHTML").innerHTML = datosCarrito.articles[0].currency +" "+ Math.round(subTotInicial*porcentajeEnvio);
+              document.getElementById("costoTotal").innerHTML = Math.round(subTotInicial*porcentajeEnvio + subTotInicial);
+            }
+          });
+
+
+      }
+  });
 
 });
---
+++ js/cartDesafiante.js
@@ -0,0 +1,41 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+  getJSONData(CARRITO_URL).then(function(resultObj2){
+    if (resultObj2.status === "ok")
+    {
+      datosCarrito2 = resultObj2.data;
+
+      for(i=0; i<datosCarrito2.articles.length; i++){
+        let contenidoTabla = "";
+        let moneda = "";
+        let subTotInicial2 = 0;
+        moneda = datosCarrito2.articles[i].currency;
+
+        if (moneda == "UYU"){
+          subTotInicial2 = datosCarrito2.articles[i].unitCost * datosCarrito2.articles[i].count;
+        }else{
+          subTotInicial2 = datosCarrito2.articles[i].unitCost * datosCarrito2.articles[i].count * 40;
+        };
+
+        contenidoTabla += `
+        <tr>
+          <td id="imgArticulo`+ i +`"><img src="`+datosCarrito2.articles[i].src+`" class="img-thumbnail" width="12%"></td>
+
+          <td id="nombreArticulo`+ i +`">`+datosCarrito2.articles[i].name+`</td>
+
+          <td id="costoArticulo`+ i +`">`+datosCarrito2.articles[i].currency +" "+ datosCarrito2.articles[i].unitCost+`</td>
+
+          <td id="cantidadArticulo`+ i +`"><input id="inputCantArticulos2" class="quantity" min="1" name="quantity"
+          value="`+ datosCarrito2.articles[i].count +`" type="number"></td>
+
+          <td id="subtotalArticulo`+ i +`">`+"UYU "+ subTotInicial2 +`</td>
+        </tr>
+        `;
+        document.getElementById("articlesWrapper").innerHTML += contenidoTabla;
+
+      };
+    };
+  });
+});
